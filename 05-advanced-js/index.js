f81398b3dcc422c942e25db3558a20a2736f5226
Mikhail Nikolaev <cakenumber1@mail.ru>
2021-12-10 21:38:53 +0300

    [05-advanced-js] implemented func: sequence, mybind, container

---
+++ 05-advanced-js/index.js
@@ -0,0 +1,51 @@
+function sequence (start = 0, steps = 1) {
+  let place = start
+  return function () {
+    place += steps
+    return place - steps
+  }
+}
+
+const firstTest = sequence()
+const firstTest2 = sequence(3, 6)
+console.log(firstTest()) // 0
+console.log(firstTest()) // 1
+console.log(firstTest2()) // 3
+console.log(firstTest2()) // 9
+
+// Function.prototype is read only, properties should not be added
+// sum.myBind = function ... тоже работает
+
+Function.prototype.myBind = function (context, ...args) {
+  return (...args2) => {
+    return this.apply(context, args.concat(args2))
+  }
+}
+
+function sum (...args) {
+  let ans = this.a + this.b || 0
+  for (let i = 0; i < args.length; i++) {
+    ans += args[i]
+  }
+  return ans
+}
+
+console.log(sum(null, 10, 20)) // 30
+console.log(sum.myBind({ a: 3, b: 6 }, 10, 20)()) // 39
+
+function container () {
+  const content = []
+  return (...args) => {
+    if (args.length === 0) {
+      return content
+    } else {
+      content.push(...args)
+    }
+  }
+}
+
+const thirdTest = container()
+thirdTest('asfdada')
+thirdTest('avcxx')
+thirdTest('123')
+console.log(thirdTest()) // [ 'asfdada', 'avcxx', '123' ]
