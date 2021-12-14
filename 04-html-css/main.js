596734c688ce6717729a9fb264f6d550b61799bc
Mikhail Nikolaev <cakenumber1@mail.ru>
2021-12-14 10:59:23 +0300

    [04-html-css] wrapped functions into classes

--- 04-html-css/main.js
+++ 04-html-css/main.js
@@ -1,40 +1,47 @@
-let tx = document.getElementsByClassName('comment-create__comment-text');
+class TexareaResize {
+  constructor() {
+    this.tx = document.getElementsByClassName('comment-create__comment-text');
+    for (let i = 0; i < this.tx.length; i++) {
+      this.tx[i].setAttribute('style', 'height:' + (this.tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+      this.tx[i].addEventListener('input', this.onInput, false);
+    }
+  }
 
-
-for (let i = 0; i < tx.length; i++) {
-  tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
-  tx[i].addEventListener("input", onInput, false);
+  onInput() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+  }
 }
 
-function onInput() {
-  this.style.height = 'auto';
-  this.style.height = (this.scrollHeight) + 'px';
+class LikeUnlike {
+  constructor() {
+    this.likes = document.getElementsByClassName('like');
+    for (let i = 0; i < this.likes.length; i++) {
+      this.likes[i].addEventListener('click', this.like);
+    }
+  }
+
+  like() {
+    let lk = this.getElementsByClassName('like-color')[0];
+    let c = this.getElementsByClassName('likes__amount')[0];
+    if (lk.getAttribute('fill') == 'red') {
+      lk.setAttribute('fill', 'currentColor')
+      c.innerText = ((parseInt(c.innerText) - 1) > 0) ? parseInt(c.innerText) - 1 : 0
+    } else {
+      lk.setAttribute('fill', 'red')
+      c.innerText = parseInt(c.innerText) + 1
+    }
+  }
 }
 
 function showComments() {
   let sc = document.getElementsByClassName('show-comments')[0];
   let coms = document.getElementsByClassName('comment');
-  sc.style.display ='none'
+  sc.style.display = 'none'
   for (let i = 0; i < coms.length; i++) {
     coms[i].style.display = 'flex'
   }
 }
 
-let likes = document.getElementsByClassName('like');
-for (let i = 0; i < likes.length; i++) {
-  likes[i].addEventListener("click", like);
-}
-
-function like() {
-  let lk = this.getElementsByClassName('like-color')[0];
-  let c = this.getElementsByClassName('likes__amount')[0];
-  if (lk.getAttribute('fill') == 'red') {
-    lk.setAttribute('fill', 'currentColor')
-    c.innerText = ((parseInt(c.innerText) - 1) > 0) ? parseInt(c.innerText) - 1: 0
-  }
-  else{
-    lk.setAttribute('fill', 'red')
-    c.innerText = parseInt(c.innerText) + 1
-  }
-}
-
+let texareaResize = new TexareaResize();
+let likeUnlike = new LikeUnlike();
