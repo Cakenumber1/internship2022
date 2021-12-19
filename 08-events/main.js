dd2e2d5aa3494a34503a20e9b7c8682319b83b21
Mikhail Nikolaev <cakenumber1@mail.ru>
2021-12-19 07:11:52 +0300

    [08-events] implemented random solvable matrix generation

--- 08-events/main.js
+++ 08-events/main.js
@@ -1,8 +1,8 @@
 const ans = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, null]]
-const matrix = [[8, 11, 7, 12], [3, 15, 6, 10], [2, 5, 13, 14], [9, 1, 4, null]]
+const matrix = generateMatrix([8, 11, 7, 12, 3, 15, 6, 10, 2, 5, 13, 14, 9, 1, 4, null])
 const table = document.getElementById('table')
 const tableInner = table.children[0].children
-let emptyPlace = [3, 3]
+let emptyPlace
 
 table.addEventListener('click', (e) => {
   const targetPlace = [e.target.parentElement.rowIndex, e.target.cellIndex]
@@ -15,29 +15,37 @@ table.addEventListener('click', (e) => {
   } else if ((Math.abs(diffY) === 1) && (diffX === 0)) {
     param = `translateY(${-diffY * 57}px)`
   } else {
-    e.target.classList.add('shake')
-    setTimeout(() => { e.target.classList.remove('shake') }, 200)
+    if (e.target.innerText !== '') {
+      e.target.classList.add('shake')
+      setTimeout(() => {
+        e.target.classList.remove('shake')
+      }, 200)
+    }
   }
   if (param) {
-    move(e.target, empty, param)
-    swap(targetPlace)
-    setTimeout(() => { drawTable(e.target, empty) }, 10)
-    setTimeout(() => { if (checkSolved()) alertWin() }, 300)
+    moveElement(e.target, empty, param)
+    swapInMatrix(targetPlace)
+    setTimeout(() => {
+      swapInWindow(e.target, empty)
+    }, 10)
+    setTimeout(() => {
+      if (checkSolved()) alertWin()
+    }, 300)
   }
 })
 
-function move (targetElem, emptyElem, param) {
+function moveElement (targetElem, emptyElem, param) {
   targetElem.style.transform = param
 }
 
-function swap (targetPlace) {
+function swapInMatrix (targetPlace) {
   const temp = matrix[targetPlace[0]][targetPlace[1]]
   matrix[targetPlace[0]][targetPlace[1]] = null
   matrix[emptyPlace[0]][emptyPlace[1]] = temp
   emptyPlace = [targetPlace[0], targetPlace[1]]
 }
 
-function drawTable (targetElem, emptyElem) {
+function swapInWindow (targetElem, emptyElem) {
   const temp = targetElem.innerText
   targetElem.innerText = ''
   targetElem.style.transform = ''
@@ -51,11 +59,15 @@ function checkSolved () {
 }
 
 function alertWin () {
-  // TODO: smth
+  // TODO: esli budet vrem9 write smth beautiful
   alert('You win')
 }
 
-function checkSolvable () {
+function solve () {
+  // TODO: esli budet vrem9 write algo that solves task
+}
+
+function checkUnsolvable (matrix) {
   let sum = 4
   const temp = matrix[0].concat(matrix[1]).concat(matrix[2]).concat(matrix[3])
   for (let i = temp.length - 1; i >= 0; i--) {
@@ -65,5 +77,55 @@ function checkSolvable () {
       }
     }
   }
-  return sum
+  return sum & 1
+}
+
+function generateMatrix (matrix) {
+  const array = matrix
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  const ans = []
+  for (let i = 0; i < 4; i++) {
+    ans.push(array.slice(i * 4, (i + 1) * 4))
+  }
+  while (checkUnsolvable(ans)) rotateMatrix(ans)
+  return ans
+}
+
+function rotateMatrix (matrix) {
+  const l = matrix[0].length
+  for (let i = 0; i < l; i++) {
+    for (let j = i; j < l; j++) {
+      const temp = matrix[j][i]
+      matrix[j][i] = matrix[i][j]
+      matrix[i][j] = temp
+    }
+  }
+  for (let i = 0; i < l; i++) {
+    for (let j = 0, k = l - 1; j < k; j++, k--) {
+      const temp = matrix[j][i]
+      matrix[j][i] = matrix[k][i]
+      matrix[k][i] = temp
+    }
+  }
+}
+
+function drawMatrix () {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const elem = document.createElement('td')
+      elem.classList.add('p-3')
+      if (!matrix[i][j]) {
+        elem.classList.add('table-active')
+        emptyPlace = [i, j]
+      } else {
+        elem.innerText = matrix[i][j]
+      }
+      tableInner[i].appendChild(elem)
+    }
+  }
 }
