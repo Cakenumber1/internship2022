8bf348b9ddb5e2b909c29c99aa617e0cc76aa7ef
Mikhail Nikolaev <cakenumber1@mail.ru>
2021-12-21 09:18:37 +0300

    [09-xhr] visual & naming changes

--- 09-xhr/main.js
+++ 09-xhr/main.js
@@ -1,22 +1,23 @@
 const xhr = new XMLHttpRequest()
 const abortBtn = document.getElementById('abortButton')
 const sendBtn = document.getElementById('sendButton')
-let avatarScr = document.getElementById('avatar')
+const avatar = document.getElementById('avatar')
 
 function sendRequest () {
   const inputText = document.getElementById('input').value
-  avatarScr.src = './assets/3031256.png'
+  avatar.src = './assets/3031256.png'
   abortBtn.classList.toggle('active')
   sendBtn.classList.toggle('active')
   xhr.open('GET', `https://api.github.com/users/${inputText}`, true)
 
   xhr.onload = function () {
+    const ans = JSON.parse(this.responseText)
     if (this.status === 200) {
-      avatarScr.src = './assets/3031256.png'
-      avatarScr.src = JSON.parse(this.responseText).avatar_url
+      avatar.src = './assets/3031256.png'
+      avatar.src = ans.avatar_url
     } else {
-      avatarScr.src = ''
-      setTimeout(() => { alert(JSON.parse(this.responseText).message) }, 100)
+      avatar.src = ''
+      setTimeout(() => { alert(ans.message) }, 100)
     }
     abortBtn.classList.toggle('active')
     sendBtn.classList.toggle('active')
