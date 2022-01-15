381d281f0809d4dd10e651921371405386b7e5c9
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-01-15 04:04:21 +0300

    [14-react] updated NavBarComponent

--- 13-react/src/components/NavBarComponent/NavBarComponent.jsx
+++ 13-react/src/components/NavBarComponent/NavBarComponent.jsx
@@ -1,10 +1,45 @@
+import {
+  Button,
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+
 function NavBarComponent({user}) {
   return (
-    <div className="NavBarComponent">
-      <h1>Blog App</h1>
-      <div>{user.username}</div>
-      <button>Logout</button>
-    </div>
+    <Navbar
+      color="light"
+      expand="md"
+      fixed=""
+      full
+      light
+    >
+      <NavbarBrand href="/">
+        Blog App
+      </NavbarBrand>
+      <NavbarToggler onClick={function noRefCheck(){}} />
+      <Collapse navbar>
+        <Nav
+          className="me-auto"
+          navbar
+        >
+          <NavItem>
+            <NavLink href={`\\${user.username}`}>
+              {user.username}
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <NavbarText>
+          <Button>Logout</Button>
+        </NavbarText>
+      </Collapse>
+    </Navbar>
   );
 }
 
