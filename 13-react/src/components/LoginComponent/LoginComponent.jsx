7f567ddcde74c3a325672630e980d536604ab7e8
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-01-21 02:47:09 +0300

    [14-react] removed convertation to css

--- 13-react/src/components/LoginComponent/LoginComponent.jsx
+++ 13-react/src/components/LoginComponent/LoginComponent.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import css from '../LoginComponent/LoginComponent.module.css'
+import css from '../LoginComponent/LoginComponent.module.scss'
 
 function LoginComponent({login}) {
 
