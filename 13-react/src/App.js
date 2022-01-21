1b5b91512461a3812c5b5e2dfa11be4db40ffad4
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-01-21 15:13:10 +0300

    [16-react] #1 #3 impl Routing in App.js Impl constants.js with routes Impl authentication.js updated package.json

--- 13-react/src/App.js
+++ 13-react/src/App.js
@@ -5,10 +5,18 @@ import data_articles from './articles.json'
 import data_notifications from './notifications.json'
 import FeedContainer from './containers/FeedContainer/FeedContainer';
 import NotificationContainer2 from './containers/NotificationContainer/NotificationContainer2';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AuthLayoutComponent from './components/AuthLayoutComponent/AuthLayoutComponent';
+import LoginComponent from './components/LoginComponent/LoginComponent';
+import { routes } from './constants';
+import RegisterComponent from './components/RegisterComponent/RegisterComponent';
+import CommonLayoutComponent from './components/CommonLayoutComponent/CommonLayoutComponent';
+import { isAuthenticated } from './authentication';
+import NotFoundPageComponent from './components/NotFoundPageComponent/NotFoundPageComponent';
 
 const title = 'title123'
 const content = 'contentText123'
-const user = {
+const author = {
   username: 'Oleg',
 }
 const createdAt = Date().toLocaleString()
@@ -20,30 +28,42 @@ function fetchUser() {
   }
 }
 
-//height for test
 function App() {
   return (
-    <div className="App" style={{height: '2000px'}}>
-      <AppFunc fetchUser={fetchUser}/>
-      <ArticleOverviewComponentFunc
-        title={title}
-        content={content}
-        user={user}
-        createdAt={createdAt}
-        imageUrl={imageUrl}
-      />
-      <FeedContainer fetchArticles={() => {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            resolve(data_articles);
-          }, 1000)
-        })
-      }}/>
-      <NotificationContainer2 fetchNotifications={() => {
-        return new Promise((resolve) => {
-          resolve(data_notifications)
-        })
-      }}/>
+    <div className="App">
+      <Router>
+        {(isAuthenticated() ? <AppFunc fetchUser={fetchUser}/> : null)}
+        <Switch>
+          <AuthLayoutComponent exact path={routes.LOGIN} component={LoginComponent}
+                               isAuthenticated={isAuthenticated()}/>
+          <AuthLayoutComponent exact path={routes.REGISTER} component={RegisterComponent}
+                               isAuthenticated={isAuthenticated()}/>
+          <CommonLayoutComponent exact path={routes.FEED} component={FeedContainer}
+                                 isAuthenticated={isAuthenticated()}
+                                 fetchArticles={() => {
+                                   return new Promise((resolve) => {
+                                     setTimeout(() => {
+                                       resolve(data_articles);
+                                     }, 1000)
+                                   })
+                                 }}/>
+          <CommonLayoutComponent exact path={routes.ARTICLE} component={ArticleOverviewComponentFunc}
+                                 isAuthenticated={isAuthenticated()}
+                                 title={title}
+                                 content={content}
+                                 user={author}
+                                 createdAt={createdAt}
+                                 imageUrl={imageUrl}
+          />
+          <Route component={NotFoundPageComponent}/>
+        </Switch>
+      </Router>
+      {(isAuthenticated()) ?
+        <NotificationContainer2 fetchNotifications={() => {
+          return new Promise((resolve) => {
+            resolve(data_notifications)
+          })
+        }}/> : null}
     </div>);
 }
 
---
+++ 13-react/src/constants.js
@@ -0,0 +1,6 @@
+export const routes = {
+  LOGIN: '/login',
+  REGISTER: '/register',
+  FEED: '/feed',
+  ARTICLE: '/article'
+}
