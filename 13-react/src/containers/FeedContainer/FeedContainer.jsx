0f88c3b0822d60e530c3bbce3c53f2fb084c3b62
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-01-21 03:34:30 +0300

    [15-react] #10 implemented FeedContainer.jsx

---
+++ 13-react/src/containers/FeedContainer/FeedContainer.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import FeedComponent from '../../components/FeedComponent/FeedComponent';
+
+function FeedContainer({fetchArticles}) {
+
+  const [articles, setArticles] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetchArticles().then(e => {
+      setArticles(e.results)
+      setLoading(false)
+    })
+  }, [fetchArticles])
+  if(!loading){
+    //<FeedComponent articles={null}/>
+    return (
+      <FeedComponent articles={articles}/>
+    );
+  }
+  else {
+    return (
+      <>loader</>
+    )
+  }
+}
+
+export default FeedContainer;
