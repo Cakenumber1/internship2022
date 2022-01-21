192d8036dff3902a9d09e95fc9cf65d36f1a7fe7
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-01-21 03:34:04 +0300

    [15-react] #9 implemented CardComponent.jsx & FeedComponent.jsx

---
+++ 13-react/src/components/CardComponent/CardComponent.jsx
@@ -0,0 +1,8 @@
+function CardComponent({data}) {
+
+  return (
+    <div className="CardComponent">{data}</div>
+  );
+}
+
+export default CardComponent;
---
+++ 13-react/src/components/FeedComponent/FeedComponent.jsx
@@ -0,0 +1,19 @@
+import CardComponent from '../CardComponent/CardComponent';
+
+function FeedComponent({articles}) {
+  if (articles) {
+    return (
+      <div>
+        {articles.map(card => (
+          <CardComponent key={card.name} data={card.name}/>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>No articles</div>
+    )
+  }
+}
+
+export default FeedComponent;
