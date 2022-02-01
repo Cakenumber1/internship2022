d2ef8ce8c8636a58e5ed1ec9ee589c4dac04df67
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-02-01 13:20:39 +0300

    [21-nodejs] fixed options for --web impl copy func in services.js renamed writeFunc.js to webServer.js rewrote readAndWrite func according to new options rm readAndWriteLocal func in writeFunc.js

--- 21-nodejs/index.js
+++ 21-nodejs/index.js
@@ -1,21 +1,22 @@
-const {readAndWriteWeb, readAndWriteLocal} = require('./writeFunc');
+const {readAndWrite} = require('./webSever');
+const {copy} = require('./services');
 const {argv} = require('yargs')
     .help('h')
     .alias('h', 'help')
     .example('$0 --from ./test.txt --to ./qwe.txt',
         'Скопирует данные из test.txt в qwe.txt локально')
-    .example('$0 --from ./test.txt --to ./qwe.txt --web',
-        'Скопирует данные из test.txt в qwe.txt через web-сервер')
+    .example('$0 --to ./qwe.txt --web',
+        'Скопирует данные из body POST запроса в qwe.txt через web-сервер')
     .option('web', {describe: 'Запустить в режиме web-сервера'})
     .option('from', {describe: 'Путь откуда', default: './'})
     .option('to', {describe: 'Путь куда', default: './'})
     .check((argv) => {
       const {from = './', to = './'} = argv;
       if (argv.web) {
-        readAndWriteWeb(from, to);
+        readAndWrite(to);
         return true;
       } else {
-        readAndWriteLocal(from, to);
+        copy(from, to);
         return true;
       }
     });
--- 21-nodejs/services.js
+++ 21-nodejs/services.js
@@ -1,9 +1,22 @@
 const util = require('util');
 const fs = require('fs');
 const fsWriteFile = util.promisify(fs.writeFile);
+const fsReadFile = util.promisify(fs.readFile);
 
 async function write(to, data) {
   await fsWriteFile(to, data);
 }
+async function read(from) {
+  return await fsReadFile(from, 'utf-8');
+}
+async function copy(from, to) {
+  try {
+    const data = await read(from);
+    await write(to, data);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 exports.write = write;
+exports.copy = copy;
