d335d40b19f5aafa232b890c7acadcc0e3d78c53
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-02-04 04:47:45 +0300

    [22-nodejs] impl version to store data beyond sessions

--- 22-nodejs/src/articles/articles.service.ts
+++ 22-nodejs/src/articles/articles.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
-import articlesData from '../fakeDB/initialArticles';
+import articlesData from '../fakeDB/data.json';
+import write from "../services/writeFile.service";
 
 @Injectable()
 export class ArticlesService {
   create(createArticleDto: CreateArticleDto) {
     articlesData.results.push(createArticleDto);
+    write('./src/fakeDB/data.json', JSON.stringify(articlesData));
     return articlesData.results;
   }
 
